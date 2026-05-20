f6f1d40346f560b3439cd9ae627fd4f4905fc96a
Boris Karabut <b03246599@gmail.com>
2026-05-20 10:30:45 +0300

    metrics: Fixed google metrics

--- public/static/googleMetric.js
+++ public/static/googleMetric.js
@@ -1,19 +1,20 @@
-// Google Analytics counter
-(function () {
-  var script = document.createElement("script");
-  script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js?id=G-1VK6FRKG6W";
+// Google Tag Manager
+(function(w,d,s,l,i){
+  w[l]=w[l]||[];
+  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+  var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),
+      dl=l!='dataLayer'?'&l='+l:'';
+  j.async=true;
+  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+  f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WXF4GGNP');
 
-  var firstScript = document.getElementsByTagName("script")[0];
-  firstScript.parentNode.insertBefore(script, firstScript);
-
+// gtag функция для удобства (опционально)
+window.gtag = function(){
   window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
+  window.dataLayer.push(arguments);
+};
 
-  gtag("js", new Date());
-
-  gtag("config", "G-1VK6FRKG6W");
-})();
-// Google Analytics counter
+gtag('js', new Date());
+gtag('config', 'GTM-WXF4GGNP');
